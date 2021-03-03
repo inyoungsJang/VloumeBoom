03af3562e67342f20be81ba8855badca08e3027e
inyoungsJang <zxzxdlsdud@naver.com>
2021-03-03 22:05:26 +0900

    Initial commit

---
+++ app/src/main/java/com/example/vloumeboom/Main_RecyclerView_Item.java
@@ -0,0 +1,14 @@
+package com.example.vloumeboom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main_RecyclerView_Item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_recycler_view__item);
+    }
+}
