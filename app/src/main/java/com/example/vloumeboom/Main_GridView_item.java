3c356ee020f0825d2b436bfa3c82e2a4cf88bbbc
inyoungsJang <zxzxdlsdud@naver.com>
2021-03-04 16:58:59 +0900

    리사이클러뷰안의 그리드뷰아이템 생성

---
+++ app/src/main/java/com/example/vloumeboom/Main_GridView_item.java
@@ -0,0 +1,14 @@
+package com.example.vloumeboom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main_GridView_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__grid_view_item);
+    }
+}
